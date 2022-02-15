e9b4a6a0801b1c3f850744615fc284ea5374d854
Ataklete <65136939+Ataklete@users.noreply.github.com>
2022-02-15 13:27:32 -0500

    update stream

--- src/Exersice6.java
+++ src/Exersice6.java
@@ -1,13 +1,13 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import com.sun.scenario.effect.Identity;
+
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Exersice6 {
     public static void main(String[] args) {
         List<Integer> stream = new ArrayList<>(Arrays.asList(1, 2, 2, 5, 6, 8, 8, 4, 4, 4, 3, 9, 9));
+        Map<Integer,Integer> map = new HashMap<>();
         List<Integer> collect = stream.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .entrySet()
                 .stream()
@@ -15,7 +15,14 @@ public class Exersice6 {
                 .map(Map.Entry::getKey)
                 .collect(Collectors.toList());
         System.out.println(collect);
-
-
+ //-----------------------------------------------
+        List<Integer> collect1 = stream.stream().filter(i -> Collections.frequency(stream, i) > 1)
+                .collect(Collectors.toList());
+        System.out.println(collect1);
+ //---------------------------------------------------
+        long count = stream.stream()
+                .collect(Collectors.groupingBy(stream::get, Collectors.counting()))
+                .values().stream().filter(i -> i > 1).count();
+        System.out.println(count);
     }
 }
