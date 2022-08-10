0e4147b57ade342c2721dd5c56b33480ac5fda74
Ataklete <65136939+Ataklete@users.noreply.github.com>
2022-08-10 17:49:22 -0400

    Update Overloading main class

---
+++ src/OverLoading.java
@@ -0,0 +1,30 @@
+public class  OverLoading{
+
+        int add(int a, int b) {
+            return (a+b);
+        }
+
+        public static void main (String args[]) {
+            System.out.println(" using OverLoading class");
+            OverLoading a = new OverLoading();
+            System.out.println("OverLoading is :" + a.add(5,10));
+        }
+
+        public static void main (int i) {
+            System.out.println(" Using OverLoading class main function with integer argument");
+            OverLoading a = new OverLoading();
+            System.out.println("OverLoading is :" + a.add(20, 10));
+        }
+    }
+
+    class DefClass {
+
+        public static void main (String args[]) {
+            System.out.println(" using DefClass");
+            OverLoading a = new OverLoading();
+            System.out.println("OverLoading is :" + a.add(5, 10));
+            OverLoading.main(null);
+            OverLoading.main(1);
+        }
+    }
+
