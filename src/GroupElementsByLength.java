d8bce8d02635cd0724793c8573b1686a86eefec0
Ataklete <65136939+Ataklete@users.noreply.github.com>
2023-11-26 20:45:13 -0600

    GroupElementsByLength Class

--- src/GroupElementsByLength.java
+++ src/GroupElementsByLength.java
@@ -5,19 +5,26 @@ import java.util.stream.Collectors;
 
 public class GroupElementsByLength {
     public static void main(String[] args) {
-  // Q1 Group array element based on the length of values ->OUTPUT: 4=[pear], 5=[apple, grape], 6=[banana]
-        List<String> str = List.of("apple","banana","pear","grape");
-        // Option 1
+        // Q1 Group array element based on the length of values ->OUTPUT: 4=[pear], 5=[apple, grape], 6=[banana]
+        List<String> str = List.of("apple", "banana", "pear", "grape", "blueberry");
+        // Option one
         System.out.println(str.stream()
-                .collect(Collectors.groupingBy(a->a.length(),Collectors.toList()))
+                .collect(Collectors.groupingBy(a -> a.length(), Collectors.toList()))
                 .entrySet());
-        // Option 2
+        // Option two
         System.out.println(str.stream().collect(Collectors.groupingBy(String::length)));
 
-  // Q2 Join all the words from the list into a single string
-  // input list : ["Hello", " ", "world", "!"]
+        // Q2 find the first element starting with character b
+        System.out.println(str.stream().filter(c->c.startsWith("b")).findFirst().orElse("Not find in the list"));
+
+        // Q3 Join all the words from the list into a single string
+        // input list : ["Hello", " ", "world", "!"]
         List<String> strList = Arrays.asList("Hello", " ", "world", "!");
-        System.out.println(strList.stream().reduce("",(x,y)-> x + y));
+        System.out.println(strList.stream().reduce("", (x, y) -> x + y));
         System.out.println(strList.stream().collect(Collectors.joining()));
-          }
+
+        // Q4 Print if all are even or odd
+        List<Integer> num = Arrays.asList(2, 4, 6, 8);
+        System.out.println(num.stream().allMatch(n -> n % 2 == 0));
+    }
 }
