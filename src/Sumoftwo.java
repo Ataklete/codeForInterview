24f65ffafe0faba42f41d3dfaafd18235acf712f
Ataklete <65136939+Ataklete@users.noreply.github.com>
2023-12-02 10:07:03 -0600

    Sumoftwo Class

--- src/Sumoftwo.java
+++ src/Sumoftwo.java
@@ -12,10 +12,7 @@ import java.util.stream.Stream;
   output: -2, 1*/
 public class Sumoftwo {
 
-    public static void main(String[] args) {
-
-        int[] arr = {2, 8, 7, 0, -1, -5, 3, -2};
-        int[] arr1 = {13, 1, 4, 6, -2, -9, -20};
+    public static void sumToNumbers(int[] arr){
 
         Map<Integer, List<Integer>> map = new TreeMap<>();
         for (int i = 0; i <= arr.length - 1; i++) {
@@ -24,6 +21,13 @@ public class Sumoftwo {
             }
         }
         System.out.println(map.entrySet().stream().filter(x -> x.getKey() >= 0).findFirst().get().getValue());
+    }
+    public static void main(String[] args) {
+
+        int[] arr = {2, 8, 7, 0, -1, -5, 3, -2};
+        int[] arr1 = {13, 1, 4, 6, -2, -9, -20};
+        sumToNumbers(arr);
+        sumToNumbers(arr1);
         }
     }
 
