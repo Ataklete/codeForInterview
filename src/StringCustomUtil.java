b85a99247b5a67e36a3835414bbf0be990b36881
Ataklete <65136939+Ataklete@users.noreply.github.com>
2023-12-06 11:39:29 -0600

    StringCustomUtil Class

--- src/StringCustomUtil.java
+++ src/StringCustomUtil.java
@@ -1,9 +1,7 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /*
 * A sentence is a string of single-space separated words where each word consists only of lowercase letters.
@@ -36,44 +34,34 @@ All the words in s1 and s2 are separated by a single space.
 
 */
 public class StringCustomUtil {
-    public String[] testString(String s1, String s2) {
+    public static String[] testString(String s1, String s2) {
         if (s1.length() < 1 && s2.length() <= 200) {
             return null;
         }
         String[] newS1 = s1.trim().toLowerCase().split("\\s+");
         String[] newS2 = s2.trim().toLowerCase().split("\\s+");
-        Map<String, Integer> map = new HashMap<>();
         List<String> list = new ArrayList<>();
         int count = 0;
-        if (newS1.length != 1) {
             for (int i = 0; i < newS1.length; i++) {
-                if (!map.containsKey(newS1[i])) {
-                    map.put(newS1[i], 1);
+                if (!list.contains(newS1[i])) {
+                    list.add(newS1[i]);
                 } else {
-                    int a = map.get(newS1[i]);
-                    map.put(newS1[i], a + 1);
+                   list.remove(newS1[i]);
                 }
             }
             for (int i = 0; i < newS2.length; i++) {
-                if (!map.containsKey(newS2[i])) {
-                    map.put(newS2[i], 1);
+                if (!list.contains(newS2[i])) {
+                    list.add(newS2[i]);
                 } else {
-                    int a = map.get(newS2[i]);
-                    map.put(newS2[i], a + 1);
+                    list.remove(newS2[i]);
                 }
             }
 
-            for (Map.Entry<String, Integer> a : map.entrySet()) {
-                if (a.getValue() == 1) {
-                    list.add(a.getKey());
-                }
-            }
-            String[] result = new String[list.size()];
-            for (String s : list){
-                result[count++] = s;
-            }
+            String[] result = list.toArray(String[]::new);
             return result;
-        }
-        return new String[]{newS1[0],newS2[0]};
+    }
+
+    public static void main(String[] args) {
+        Arrays.stream(testString("ati ati helen henok", "saba saba")).forEach(x -> System.out.println(x));
     }
 }
